93f612c4111cd95e89224a241c87114e68269632
Brandi Anderson <branderso87@gmail.com>
2017-12-06 16:52:17 -0600

    add cookieSession & use passport in index.js

--- server/index.js
+++ server/index.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
 const keys = require('./config/keys')
+require('./models/User')//should pull in models before we run passport
 require('./services/passport')
-require('./models/User')
 
 mongoose.connect(keys.mongoURI)
 
 const app = express()
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, //30 days in milliseconds
+    keys:[keys.cookieKey]
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
+
 require('./routes/authRoutes')(app)
 
 
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,22 @@
 const passport = require('passport')
 const googleStrategy = require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose')
 const keys = require('../config/keys')
+
+const User = mongoose.model('users')
+
+//take user model and turn into identifiable key for cookie
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+//turn key abck into user
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user)
+    })
+})
 
 passport.use(
   new googleStrategy({
@@ -9,8 +25,17 @@ passport.use(
     callbackURL: '/auth/google/callback' //redirect to this route after authorization
   },
   (accessToken, refreshToken, profile, done) => {
-    console.log('accessToken', accessToken)
-    console.log('refreshToken', refreshToken)
-    console.log('profile', profile)
+    User.findOne({googleID: profile.id})
+      .then((existingUser) => {
+        if(existingUser) {
+          //we already have a record with the given profile ID
+          done(null, existingUser)
+        }else {
+          //we don't have a user record ith this ID, make a new record
+          new User({googleID: profile.id})
+            .save()
+            .then(user => done(null, user)) //verified that it was saved to DB and pushes to serializeUser
+        }
+    })
   })
 )
