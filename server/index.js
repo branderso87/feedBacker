4ff6a17a368d1a8c029a06dc275c74adf225b470
Brandi Anderson <branderso87@gmail.com>
2017-12-06 13:35:32 -0600

    added keys.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const passport = reuqire('passport')
+const googleStrategy = require('passport-google-oauth20').Strategy
+
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send({poop:'poopy'})
-})
+passport.use(new googleStrategy())
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
