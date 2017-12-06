7460dc3e27ef48689873dd1b4236eb7797c94ef3
Brandi Anderson <branderso87@gmail.com>
2017-12-06 17:03:58 -0600

    added logout; authentication flow working

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -6,4 +6,13 @@ module.exports = (app) => {
   })
   )
   app.get('/auth/google/callback', passport.authenticate('google'))//there is a code now attatched to the callback url to be used to grab profile from google
+
+  app.get('/api/logout', (req, res) => {
+    req.logout()
+    res.send(req.user)
+  })
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user)
+  })
 }
