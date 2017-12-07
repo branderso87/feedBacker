76ecfee746a4aac99dc6135daf106d6ecf46d94e
Brandi Anderson <branderso87@gmail.com>
2017-12-07 01:17:04 -0600

    refactor fetchUser

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
 import {FETCH_USER} from './types'
 
-export const fetchUser = () => {//whenever the action creator is called...
-  return function(dispatch) {//it returns a function that is seen my redux thunk that calls it with dispatch
-    axios.get('/api/current_user')//we wait to get response back from the api
-    .then(res => dispatch({ type: FETCH_USER, payload: res}))//only then do we send to dispatch which will send action to the reducers
+export const fetchUser = () => //whenever the action creator is called...
+  async (dispatch) => {//it returns a function that is seen my redux thunk that calls it with dispatch
+    const res = await axios.get('/api/current_user')//we wait to get response back from the api
+    dispatch({ type: FETCH_USER, payload: res.data})//only then do we send to dispatch which will send action to the reducers
   }
-}
