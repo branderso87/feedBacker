22396a72849b1ce71359e968fecb3fa7b4402764
Brandi Anderson <branderso87@gmail.com>
2017-12-07 00:07:31 -0600

    uninstalled and then reinstalled all node_mods

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -3,11 +3,27 @@ import React, { Component } from 'react'
 class Header extends Component {
   render() {
     return (
-    <div>
-      Header
-    </div>
+      <nav>
+        <div className="nav-wrapper">
+          <a className="left brand-logo">feedBacker</a>
+          <ul className="right">
+            <li><a>Login with Google</a></li>
+          </ul>
+        </div>
+      </nav>
     )
   }
 }
 
 export default Header
+
+//  <nav>
+  //   <div class="nav-wrapper">
+  //     <a href="#" class="brand-logo">Logo</a>
+  //     <ul id="nav-mobile" class="right hide-on-med-and-down">
+  //       <li><a href="sass.html">Sass</a></li>
+  //       <li><a href="badges.html">Components</a></li>
+  //       <li><a href="collapsible.html">JavaScript</a></li>
+  //     </ul>
+  //   </div>
+  // </nav>
